b264a3427270a89fdd189c36662ba19c66ed36bf
Starmapo <starmapo@proton.me>
2024-01-02 13:34:55 -0400

    Set version to 0.1.0 - Changed update check to this repository - Updated README

--- source/KadeEngineFPS.hx
+++ source/KadeEngineFPS.hx
@@ -113,7 +113,7 @@ class KadeEngineFPS extends TextField
 		{
 			text = (FlxG.save.data.fps ? "FPS: "
 				+ currentFPS
-				+ (Main.watermarks ? "\nKE " + "v" + MainMenuState.kadeEngineVer : "") : (Main.watermarks ? "KE " + "v" + MainMenuState.kadeEngineVer : ""));
+				+ (Main.watermarks ? "\nKE Week 7 " + "v" + MainMenuState.kadeEngineVer : "") : (Main.watermarks ? "KE Week 7 " + "v" + MainMenuState.kadeEngineVer : ""));
 
 			#if (gl_stats && !disable_cffi && (!html5 || !canvas))
 			text += "\ntotalDC: " + Context3DStats.totalDrawCalls();
